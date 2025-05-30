8d3247cf8dcb142ab2e05d7bec82fa882febf135
freewins <freewings@sjtu.edu.cn>
2025-05-30 12:22:51 +0800

    Done: 修复re的一部分问题 TODO: 码风不够好，同时内存冗余，代码封装需要调整

--- main.cpp
+++ main.cpp
@@ -2,17 +2,41 @@
 #include<string>
 #include "./src/command.hpp"
 
+#ifdef DEBUG
+#include<fstream>
+#endif
 int main() {
+#ifdef DEBUG
+
+   std::string paths[] ={
+     "station_info.bpt",
+     "train_basic_info.bpt",
+     "train_stations_info.data",
+     "queue_info.bpt",
+     "user_info.data",
+     "user_tickets.bpt",
+     "user_tickets.data"
+   };
+   for (int i = 0 ; i < 7; i++) {
+     if (remove(paths[i].c_str()) == 0) {
+       std::cout << "remove ok" << std::endl;
+     }
+     else{}
+   }
+
+   freopen("../testcases/63.in", "r", stdin);
+   freopen("./out/63.out.tmp", "w", stdout);
+  #endif
 
   std::string input;
   bool exit_flag = false;
   command::Command command_;
-  // std::ios::sync_with_stdio(false);
-  // std::cin.tie(nullptr);
-  // std::cout.tie(nullptr);
+  //std::ios::sync_with_stdio(false);
+  //std::cin.tie(nullptr);
+  //std::cout.tie(nullptr);
   while (!exit_flag) {
     getline(std::cin, input);
-    command_.execute(std::move(input),exit_flag);
+    command_.execute(input,exit_flag);
   }
   return 0;
 }
